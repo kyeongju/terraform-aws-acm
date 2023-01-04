ba84ef4e703e3e659110673a16451cfaa8b0b668
kyeongju <ainimo2@gmail.com>
2023-01-04 20:33:05 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ locals {
   distinct_domain_names = coalescelist(var.distinct_domain_names, distinct(
     [for s in concat([var.domain_name], var.subject_alternative_names) : replace(s, "*.", "")]
   ))
-provider = var.provider
   # Get the list of distinct domain_validation_options, with wildcard
   # domain names replaced by the domain name
   validation_domains = local.create_certificate || local.create_route53_records_only ? distinct(
